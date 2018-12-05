3d18e71a206b26bb6957535a6baaf484dbc1a6ed
Robson Silva <robsonsilva@gmail.com>
2018-12-05 16:34:02 -0200

    Nova versão do Site usando BOOTSTRAP

---
+++ index.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css" >
+        <link rel="stylesheet" href="css/personalisado.css" >
+
+        <title>ACR - Soluções em TI</title>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-md navbar-dark fixed-top " style="background-color: orange">
+                <div class="container">
+                   
+                    <a class="navbar-brand" href="index.php"><img src="imagens/logo-acr.png" class="img-fluid "alt="Responsive image" style="image"/>ACR</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item ">
+                                <a class="nav-link" href="index.php">Home </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="sobreEmpresa.php">Sobre a empresa</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contatos.php">Contatos</a>
+                            </li>
+                        </ul>
+                        <!--  <form class="form-inline mt-2 mt-md-0">
+                              <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busca</button>
+                          </form>-->
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <main role="main">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                    <li data-target="#myCarousel" data-slide-to="4"></li>
+                    <li data-target="#myCarousel" data-slide-to="5"></li>
+                </ol>
+
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="first-slide img-thumbnail" src="imagens/carrossel/imagem1.jpg" alt="Suporte técnico">
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <h1><a class="imagem1">Suporte a sistemas computacionais</a></h1>
+                                <p class="textocarro">Oferecemos suporte aos clientes que necessitem de auxílio especializado
+                                    para utlização dos sistemas computacionais que possua</p>
+                                <p><a class="btn btn-lg btn-primary" href="#" role="button">Veja mais</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="second-slide img-fluid"  src="imagens/carrossel/imagem2.jpg" alt="Desenvolvimento">
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <h1 class="imagem2">Desenvolvimento</h1>
+                                <p class="textocarro">Manutenção em computadores pessoais, servidores, periféricos em geral! </p>
+                                <p><a class="btn btn-lg btn-primary" href="#" role="button">Veja mais</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="third-slide img-fluid"src="imagens/carrossel/imagem3.jpg" alt="Diagnostico">
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <h1 class="imagem3">Diagnóstico de problemas </h1>
+                                <p class="textocarro">Ajudamos você a escolher o item necessário ao seu computador que mais
+                                    se atenda suas necessidades, sempre procurando por melhores preços e marcas!</p>
+                                <p><a class="btn btn-lg btn-primary" href="#" role="button">Veja mais</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img class="fourth-slide img-fluid"src="imagens/carrossel/imagem4.jpg" alt="Manutenção">
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <h1 class="imagem4">Manutenção de hardware</h1>
+                                <p class="textocarro">Desenvolvemos sites e sistemas web para você e para seu negócio, faça um 
+                                    orçamento sem compromisso, você pode se surpreender!</p>
+                                <p><a class="btn btn-lg btn-primary" href="#" role="button">Veja mais</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img class="fifth-slide img-fluid"src="imagens/carrossel/imagem5.jpg" alt="Vendas">
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <h1 class="imagem4">manutenção e criação de infraestruturas de rede</h1>
+                                <p class="textocarro">Desenvolvemos sites e sistemas web para você e para seu negócio, faça um 
+                                    orçamento sem compromisso, você pode se surpreender!</p>
+                                <p><a class="btn btn-lg btn-primary" href="#" role="button">Veja mais</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img class="sixth-slide img-fluid"src="imagens/carrossel/imagem6.jpg" alt="Infraestrutura e redes">
+                        <div class="container">
+                            <div class="carousel-caption text-left">
+                                <h1 class="imagem4">Desenvolvimento</h1>
+                                <p class="textocarro">Desenvolvemos sites e sistemas web para você e para seu negócio, faça um 
+                                    orçamento sem compromisso, você pode se surpreender!</p>
+                                <p><a class="btn btn-lg btn-primary" href="#" role="button">Veja mais</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div class="jumbotron servicos">
+                <div class="container">
+                    <h2 class="display-4 text-center" style="margin-bottom:40px;">Serviços</h2>
+                    <div class="card-deck">
+                        <div class="card">
+                            <img class="card-img-top" src="imagens/cards/img01.jpg" alt="Card image cap">
+
+                            <div class="card-body text-center">
+                                <h5 class="card-title">Suporte ao usuário</h5>
+                                <p class="card-text  lead">Com a ACR você pode contar com uma equipe treinada para dar suporte a sua
+                                    empresa nas diversas tarefas do dia-a-dia dentro do munto da informática. Acessoria desde a trabalhos 
+                                    de escritório até estruturadas de dados em rede.</p>
+
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top" src="imagens/cards/img02.jpg" alt="Card image cap">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">Manutenção e infraestrutura</h5>
+                                <p class="card-text lead">Nos contamos com profissionais capacitados para efetuar a manutenção de todo o 
+                                    seu Hardware: Computadores, servidores, impressoras e estrutura de rede. Também realizamos projetos de infraestrutura
+                                    e instalação de redes de dados.</p>
+
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top" src="imagens/cards/img03.jpg" alt="Card image cap">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">Desenvolvimento de aplicações e sites</h5>
+                                <p class="card-text lead">Através da ACR você pode contar com o que há de mais moderno na criação de sites e aplicações WEB.
+                                    Oferecemos orçamentos para o desenvolvimento de seu sistema dentro das suas específicações e necessidades. Não tenha
+                                    mais sustos, cote com agente!</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.2.1.slim.min.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="js/bootstrap.min.js" ></script>
+    </body>
+</html>
